8f646968e46cf1004a8c876b2511ea8da017fd75
obmbane <obmbaneaws@gmail.com>
2024-12-14 11:47:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Weather Forcast Dashboard")
+location = st.text_input(label="Location:", placeholder="Type location")
+days_slider = st.slider('Forcast Days', max_value=7, min_value=1,help="Slide to select number of days to forecast")
+option = st.selectbox("Select Weather Options:",("Temperature","Sky"))
+
+st.subheader(f"{option} for the next {days_slider} days in the {location} area")
